7ae89689f40641cb9d813a0e4a4c00aef4b9fe5b
Christopher Jensen <murdrlol@gmail.com>
2019-02-11 23:21:56 -0600

    changed h1 tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default class IndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+              <h1 className="has-text-weight-bold is-size-2">BJJ Lifestyle Academy</h1>
             </div>
             {posts
               .map(({ node: post }) => (
